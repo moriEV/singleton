5ae2895e221de86e7653206cd1717a5d4026604c
moriEV <karpenkoev@mer.ci.nsu.ru>
2024-09-09 11:06:27 +0700

    Add files via upload

---
+++ sngleton/sngleton/Program.cs
@@ -0,0 +1,15 @@
+﻿using sngleton;
+using static System.Net.WebRequestMethods;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Servers single = Servers.Instance;
+        single.AddServer("http://yandex.ru");
+        single.GetHTTP();
+        single.GetHTTPS();
+
+
+    }
+}
---
+++ sngleton/sngleton/Servers.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sngleton
+{
+    internal class Servers
+    {
+        private static volatile Servers instance = new Servers();
+        private static object syncRoot = new Object();
+        public List<string> serverList {  get; set; }
+        protected Servers()
+        {
+            serverList = new List<string> { "https://www.google.ru" };
+        }
+        public static Servers Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+        public bool AddServer(string adress)
+        {
+            lock (syncRoot)
+            {
+                if ((adress.StartsWith("http:") || adress.StartsWith("https:")) && !serverList.Contains(adress))
+                {
+                    serverList.Add(adress);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        public void GetHTTP()
+        {
+            foreach (var server in serverList)
+            {
+                if (server.StartsWith("http:"))
+                {
+                    Console.WriteLine(server);
+                }
+            }
+        }
+        public void GetHTTPS()
+        {
+            foreach (var server in serverList)
+            {
+                if (server.StartsWith("https:"))
+                {
+                    Console.WriteLine(server);
+                }
+            }
+        }
+    }
+}
